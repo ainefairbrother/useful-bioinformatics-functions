0147afae8231d76c238376e35f6072be62aa7202
Aine Fairbrother-Browne <aine.fairbrother-browne.18@ucl.ac.uk>
2022-01-17 12:38:52 +0000

    Minor edit to comment text

--- helper-funs-to-query-api-opentargets-gh.R
+++ helper-funs-to-query-api-opentargets-gh.R
@@ -3,7 +3,7 @@
 ## Date: 01/2022
 ## I created these helper functions as wrappers for the https://genetics.opentargets.org/ API. 
 ## There are a large multiude of query designs possible with this structure, but this particular use-case queries using a variantID
-## and returns some useful information related to it, namely the nearestGene.id which I designed this query to fetch. 
+## and returns some useful information related to it, namely the nearestGene.id which I originally designed this query to grab. 
 ############################################################################################################################################
 
 #' query.opentargets.with.varIDs
